e557806f2d26400716aa4319bdf9759d973b5d1e
Equan Pr <equan.p@gmail.com>
2020-12-07 13:21:47 +0700

    buat komponen baru Home

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
+  <Home />
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
