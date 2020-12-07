583bd31fc8b5691f38be8c1f07a6ad38a5f41f45
Equan Pr <equan.p@gmail.com>
2020-12-07 16:16:27 +0700

    Dark Mode auto by browser setting

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,33 @@
 <template>
-    <div></div>
+  <div class="md:container dark:bg-gray-800">
+    <figure class="md:flex bg-gray-100 dark:bg-gray-900 p-8 md:p-0">
+      <img
+        class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+        src="/948279.jpg"
+        alt=""
+        width="384"
+        height="512"
+      />
+      <div
+        class="pt-6 md:p-8 text-center md:text-left space-y-4 "
+      >
+        <blockquote>
+          <p class="text-lg font-semibold dark:text-white">
+            “Kebebasan dalam berpikir dan menulis ulang kode sebagaimana yang
+            engkau mau, itu adalah hak khusus dari Programer.”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-cyan-600 dark:text-gray-300">Equan Pr.</div>
+          <div class="text-gray-500 dark:text-gray-400">Ki Juru Tulis, SajEniD</div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
   },
   variants: {
     extend: {},
